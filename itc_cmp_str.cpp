a5313a6ed83a0056a314ca40c7379aa6120546c5
sredamar <92477629+sredamar@users.noreply.github.com>
2021-11-07 17:06:18 +0300

    Update itc_cmp_str.cpp

--- itc_cmp_str.cpp
+++ itc_cmp_str.cpp
@@ -1,11 +1,12 @@
 #include "str_easy.h"
 
 string itc_cmp_str(string str1, string str2, int num) {
-    long long len1, len2;
-    len1 = itc_len(str1);
-    len2 = itc_len(str2);
-    if (len2 + num + 1 > len1) {
-        return itc_mysubstr(str1, 0, num) + itc_mysubstr(str2, 0, len1 - num);}
-    else {
-        return itc_mysubstr(str1, 0, num) + str2 + itc_mysubstr(str1, num + len2, len1 - num - len2);}
-return 0;}
+    long long len1 = itc_len(str1);
+    long long len2 = itc_len(str2);
+    string new_str = "";
+    new_str += itc_slice_str(str1, 0, num - 1);
+    new_str += itc_slice_str(str2, 0, (len1 - num));
+    new_str += itc_slice_str(str1, num, len1);
+    return itc_slice_str(new_str, 0, (len1 - 1));}
+
+    
