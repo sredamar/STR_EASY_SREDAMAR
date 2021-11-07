748e74943ebeb5ee6f66fb1dde91a4423bcb41dd
sredamar <92477629+sredamar@users.noreply.github.com>
2021-11-07 17:25:05 +0300

    Update itc_cmp_str.cpp

--- itc_cmp_str.cpp
+++ itc_cmp_str.cpp
@@ -7,7 +7,6 @@ string itc_cmp_str(string str1, string str2, int num) {
     new_str += itc_slice_str(str1, 0, num - 1);
     new_str += itc_slice_str(str2, 0, len1 - num);
     new_str += itc_slice_str(str1, num, len1);
-    return itc_slice_str(new_str, 0, len1 - 1);
-    return "-1";}
+    return itc_slice_str(new_str, 0, len1 - 1);}
 
     
