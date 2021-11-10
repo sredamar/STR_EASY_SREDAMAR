e65c3da66e90f8daa68d26872ee405ef33232706
sredamar <92477629+sredamar@users.noreply.github.com>
2021-11-10 22:31:59 +0300

    Update itc_find_str.cpp

--- itc_find_str.cpp
+++ itc_find_str.cpp
@@ -1,18 +1,13 @@
 #include "str_easy.h"
-
-int itc_find_str(string str1, string str2) {
-    int i1 = 0, i2 = 0;
-    long long len1, len2;
-    len1 = itc_len(str1);
-    len2 = itc_len(str2);
-    while ((i1 < len1) && (i2 < len2)) {
-        if (str1[i1] == str2[i2]) {
-            i2++;}
-        else {
-            i2 = 0;}
+int itc_find_str(string str1, string str2){
+    long long len1 = itc_len(str1), len2 = itc_len(str2), counter, i1 = 0, i2 = 0;
+    while (i1 < len1){
+        counter = 0;
+        if (str1[i1] == str2[0]){
+        for (long long i2 = 0; i2 < len2; i2++){
+            if (str1[i1 + i2] == str2[i2])
+                counter += 1;}}
+        if (counter == len2)
+            return i1;
     i1++;}
-    if (i2 == len2) {
-        return i1 - len2;}
-    else {
-        return -1;}
-return 0;}
+    return -1;}
