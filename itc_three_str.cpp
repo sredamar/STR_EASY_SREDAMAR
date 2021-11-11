dbdeee9e1eabd3d1025ca5545bcde839cc43b81b
sredamar <92477629+sredamar@users.noreply.github.com>
2021-11-11 09:57:27 +0300

    Update itc_three_str.cpp

--- itc_three_str.cpp
+++ itc_three_str.cpp
@@ -1,17 +1,20 @@
-#include "str_easy.h"
 string itc_three_str(string str1, string str2, string str3) {
-    int pos = itc_find_str(str1, str2), i = 0;
-    long long len1 = itc_len(str1);
-    long long len2 = itc_len(str2);
-    string res = "";
-    while (pos != -1) {
-        res += itc_slice_str(str1, pos, pos + len2 - 1);
-        res = str3;
-        if (pos > 0) {
-            str1 = itc_slice_str(str1, 0, pos - 1) + res + itc_slice_str(str1, pos + len2, len1 - 1);}
-        else if (pos == 0) {
-            str1 = res + itc_slice_str(str1, len2, len1 - 1);}
-        else if (str1[len1 - 1] == str2[len2 - 2]) {
-            str1 = itc_slice_str(str1, 0, len1 - len2 - 1);}
-        pos = itc_find_str(str1, str2);}
-return str1;}
+int pos = itc_find_str(str1, str2), i = 0;
+long long len1 = itc_len(str1);
+long long len2 = itc_len(str2);
+string res = "";
+while (pos != -1) {
+res = str3;
+if ((pos > 0) && (pos != len1 - len2)) {
+    str1 = itc_slice_str(str1, 0, pos - 1) + res + itc_slice_str(str1, pos + len2, len1 - 1);
+    }
+else if (pos == 0) {
+    str1 = res + itc_slice_str(str1, len2, len1 - 1);
+    }
+else if (pos == len1 - len2){
+    str1 = itc_slice_str(str1, 0, pos - 1) + res;
+    }
+    pos = itc_find_str(str1, str2);
+    }
+return str1;
+}
