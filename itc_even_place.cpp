a54cc8b8001aef1df17fcc1f2bed0e00e66c48f8
sredamar <92477629+sredamar@users.noreply.github.com>
2021-11-11 11:25:44 +0300

    Update itc_even_place.cpp

--- itc_even_place.cpp
+++ itc_even_place.cpp
@@ -2,6 +2,8 @@
 string itc_even_place(string str){
     long long i = 1;
     string str_new = "";
+    if (itc_len(str) == 1)
+        return "-1";
     while (str[i] != '\0') {
         str_new = str_new + str[i];
         i = i + 2;}
